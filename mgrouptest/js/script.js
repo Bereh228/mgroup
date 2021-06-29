ebf034feccc0573db4999bf8f811c487106c6d6f
Bereh228 <andrey.berezhko999@gmail.com>
2021-06-29 22:04:02 +0300

    added slider, don't find section

---
+++ mgrouptest/js/script.js
@@ -0,0 +1,16 @@
+new Swiper('.goods-swiper',{
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true,
+    },
+    slidesPerView:3,
+    slidesPerGroup:3,
+    mousewheel: {
+        sensitivity: 1,
+    },
+    speed: 700,
+    loop:true,
+ 
+  
+});
